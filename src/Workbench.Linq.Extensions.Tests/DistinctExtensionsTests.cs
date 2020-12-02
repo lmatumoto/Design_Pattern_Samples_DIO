87c620292f37b8ae6333b27f6da87d06f6c938b6
Leandro Matumoto <matumoto.leandro@gmail.com>
2020-12-01 21:38:36 -0300

    repositorio de estudo

--- src/Workbench.Linq.Extensions.Tests/DistinctExtensionsTests.cs
+++ src/Workbench.Linq.Extensions.Tests/DistinctExtensionsTests.cs
@@ -16,15 +16,16 @@ namespace Workbench.Linq.Extensions.Tests
         [Fact(DisplayName = "Data uma coleção com 3 objetos sendo 2 iguais então o DISTINCT com uma comparação simples deve retornar uma lista com 2 objetos.")]
         public void ListagemComItensRepetidosComparecaoSimples()
         {
+            //Arrange
             IEnumerable<PessoaFisica> pessoas = new List<PessoaFisica>()
             {
                 new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Ana", CPF = "111.111.111-11" },
                 new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Paula", CPF = "111.111.111-11" },
                 new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Ana", CPF = "222.222.222-22" }
             };
-
+            //Act
             IEnumerable<PessoaFisica> pessoasDiferentes = pessoas.Distinct(p => p.CPF);
-
+            //Assert
             Assert.NotNull(pessoasDiferentes);
             Assert.True(pessoasDiferentes.Any());
             Assert.Equal(2, pessoasDiferentes.Count());
@@ -34,15 +35,16 @@ namespace Workbench.Linq.Extensions.Tests
         [Fact(DisplayName = "Data uma coleção com 3 objetos sendo 2 iguais então o DISTINCT com uma comparação composta deve retornar uma lista com 2 objetos.")]
         public void ListagemComItensRepetidosComparecaoComposta()
         {
+            //Arrange
             IEnumerable<PessoaFisica> pessoas = new List<PessoaFisica>()
             {
                 new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Ana", CPF = "111.111.111-11" },
                 new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Paula", CPF = "111.111.111-11" },
                 new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Ana", CPF = "222.222.222-22" }
             };
-
+            //Act
             IEnumerable<PessoaFisica> pessoasDiferentes = pessoas.Distinct(p => new { p.Nome, p.NomeMae });
-
+            //Assert
             Assert.NotNull(pessoasDiferentes);
             Assert.True(pessoasDiferentes.Any());
             Assert.Equal(2, pessoasDiferentes.Count());
